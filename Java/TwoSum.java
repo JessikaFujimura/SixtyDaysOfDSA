4c463e9844330bc3827203d32751cb94a56a6928
Jessika Mayumi Fujimura <jessika@Jessikas-MacBook-Air.local>
2023-07-05 23:15:25 -0300

    resolution two sum

---
+++ Java/TwoSum.java
@@ -0,0 +1,18 @@
+class TwoSum(){
+
+    private int target;
+    private [int] nums;
+
+    public TwoSum(target, nums){
+        this.target = target;
+        this.nums = nums;
+    }
+
+    public static void sumTwo(){
+        for(int i = 0; i < this.nums; i++) {
+            int rest = this.rest - nums[i];
+            for(int j = i; )
+        }
+    }
+
+}
---
+++ TypeScript/1-TwoSum.ts
@@ -0,0 +1,21 @@
+// Two Sum
+// Given an array of integers nums and an integer target , return indices of the two numbers such that they add up to target
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+const nums = [1,2,3,4,0,5];
+const target = 7;
+
+function sumTwo(nums, target){
+    let result;
+    for(let i = 0; i < nums.length ; i++){
+        const rest = target - nums[i];
+        const pos2 = nums.findIndex(j => j === rest);
+        if(pos2 !== -1){
+            result = [i, pos2];
+        }
+    }
+    console.log(result);
+}
+sumTwo(nums, target);
+
